8c481ad946051d9ee7a3d7beb78439cb1f21f691
dvvid-g <giraldo.jose@correounivalle.edu.co>
2025-12-09 15:59:17 -0500

    feat: Configure AuthRepository dependency injection in Hilt

--- app/src/main/java/com/univalle/inventarioapp/di/AppModule.kt
+++ app/src/main/java/com/univalle/inventarioapp/di/AppModule.kt
@@ -3,6 +3,8 @@ package com.univalle.inventarioapp.di
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.univalle.inventarioapp.data.remote.FirestoreRepository
+import com.univalle.inventarioapp.data.repository.AuthRepository
+import com.univalle.inventarioapp.data.repository.AuthRepositoryImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -26,5 +28,11 @@ object AppModule {
     fun provideFirestoreRepository(
         firestore: FirebaseFirestore
     ): FirestoreRepository = FirestoreRepository(firestore)
+
+    @Provides
+    @Singleton
+    fun provideAuthRepository(
+        auth: FirebaseAuth
+    ): AuthRepository = AuthRepositoryImpl(auth)
 }
 
